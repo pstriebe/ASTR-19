d83fd80495430128c36999e3faa00cc4b08920e0
pstriebe <pstriebe@ucsc.edu>
2024-10-03 12:37:51 -0700

    Code Journal #1 Prompt 1

---
+++ Code_Journal#1_Prompt#1.py
@@ -0,0 +1,15 @@
+#Code Journal #1
+
+#Prompt #1
+
+#defining Prompt 1
+def Prompt1():
+    print('My name is Peter Hermann Striebeck \n \nMy Pronouns are he/him \n \nMy favorite move is Spider-Man Into the Spiderverse and my favorite food is Sushi')
+
+#defining main
+def main():
+    Prompt1()
+    
+#executing Prompt 1 first
+if __name__ == "__main__":
+    main()
